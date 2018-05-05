bc66686cc02e770e2fa557d25de2fac803414f40
Juan Navarro <jdna0615@gmail.com>
2018-05-05 19:33:10 -0400

    mongo

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/ServicioStats-0.0.1-SNAPSHOT.jar
--- src/main/java/com/stats/conexiones/ConexionMongo.java
+++ src/main/java/com/stats/conexiones/ConexionMongo.java
@@ -15,15 +15,13 @@ import org.springframework.stereotype.Service;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
-import com.mongodb.MongoCredential;
 
 @Service
 public class ConexionMongo {
 /*
 	private final static String HOST = "localhost";
-	private final static int PORT = 27017;
+	private final static int PORT = 13700;
 	
 	MongoClient mongoClient;
 	DB db;
@@ -39,16 +37,35 @@ public class ConexionMongo {
 	public void CerrarConexion() {
 		mongoClient.close();
 	}
-	*/
-	
+*/
+
+	MongoClient mongoClient;
+	DB db;
+
 	public DB CrearConexion() throws UnknownHostException {
+//        MongoClientURI uri  = new MongoClientURI("mongodb://super18:super18@ds113700.mlab.com:13700/notable");
+        MongoClientURI uri  = new MongoClientURI(System.getenv("MONGOHQ_URL")); 
+        MongoClient client = new MongoClient(uri);
+        db = client.getDB(uri.getDatabase());
+		
+		return db;
+	}
+	
+/*	
+	public DB CrearConexion() throws UnknownHostException {
+		System.out.println(System.getenv("PATH"));
+
+		
 	    String webPort = System.getenv("PORT");
 	    if(webPort == null || webPort.isEmpty()) {
 	        webPort = "8080";
 	    }
    
+	    String url = "mongodb://super18:super18@ds113700.mlab.com";
+	    
 	    MongoClientURI uri = new MongoClientURI(System.getenv("MONGOHQ_URL"));
-	    MongoClient mongoClient = new MongoClient(uri.getURI(), Integer.valueOf(webPort));
+//	    MongoClient mongoClient = new MongoClient(uri.getURI(), Integer.valueOf(webPort));
+	    MongoClient mongoClient = new MongoClient(url, Integer.valueOf(webPort));
 	    
 	    String dbname = uri.getDatabase();
 	    DB db = mongoClient.getDB(dbname);
@@ -58,4 +75,5 @@ public class ConexionMongo {
 	    
 		return db;
 	}
+	*/
 }
