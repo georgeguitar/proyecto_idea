bee85d50485a8b6375d5286a94ae3669a2d8a78c
Juan Navarro <jdna0615@gmail.com>
2018-05-05 20:32:01 -0400

    ddd

--- src/main/java/com/stats/conexiones/ConexionMySql.java
+++ src/main/java/com/stats/conexiones/ConexionMySql.java
@@ -15,9 +15,9 @@ public class ConexionMySql {
 		try{
 			if( con == null ){
 			    String driver="com.mysql.jdbc.Driver"; //el driver varia segun la DB que usemos
-			    String url="jdbc:mysql://localhost/borrar?autoReconnect=true";
-			    String pwd="beatles";
-			    String usr="juan";
+			    String url="jdbc:mysql://mfh5oxm25hprrpon:etk70aoilrc5y8nz@ocvwlym0zv3tcn68.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g3gcrh40646vhxdn";
+			    String pwd="etk70aoilrc5y8nz";
+			    String usr="mfh5oxm25hprrpon";
 			    Class.forName(driver);
 			    con = DriverManager.getConnection(url,usr,pwd);
 			    System.out.println("Conectionesfull");
