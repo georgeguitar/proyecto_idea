41c9e71f4e9d2dfef6304c69a4c78c8eabc38ae4
Numa Navarro <lfnna1977@yahoo.es>
2018-05-06 23:30:28 -0400

    Primera subida

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    port: process.env.PORT || 3000,
+    db: process.env.MONGODB || `mongodb://super2018:super2018@ds117250.mlab.com:17250/ideas`
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./config')
+
+// Se hace la conexion y se define la base de datos a utilizar
+mongoose.connect(config.db, (err, res) => {
+    if (err) {
+        return console.log(`Error al conectar a la base de datos: ${err}`)
+    }
+    console.log('Conexion a la base de datos establecida...')
+
+    app.listen(config.port, () => {
+        console.log(`API REST corriendo en http://localhost:${config.port}`)
+    })
+})
